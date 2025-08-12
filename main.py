8e4f201e4ee1c36b1740a2cc64e385cb68df7e33
onebadduc <conjsp@gmail.com>
2025-08-12 01:40:26 -0700

    Start of game

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import *
+def main():
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 0
+    while i == 0:
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
